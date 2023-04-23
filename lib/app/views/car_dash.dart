53e55d34a90cc829134cb1f6c7ec5fc40e5c61c9
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-04-23 17:31:53 +0200

    feat: configure colors and assets 🔥

--- lib/app/views/car_dash.dart
+++ lib/app/views/car_dash.dart
@@ -7,12 +7,44 @@
 //
 
 import 'package:flutter/material.dart';
+import 'package:handy_extensions/handy_extensions.dart';
+import 'package:relative_scale/relative_scale.dart';
+
+import '../../configs/colors.dart';
 
 class CarDash extends StatelessWidget {
   const CarDash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RelativeBuilder(
+      builder: (context, height, width, sy, sx) {
+        return Container(
+          height: context.height,
+          width: context.width,
+          padding: EdgeInsets.symmetric(
+            horizontal: sx(10),
+            vertical: sy(10),
+          ),
+          decoration: const BoxDecoration(
+            color: DashColors.backgroundColor,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    child: ImageIcon(
+                      AssetImage('assets/icons/home.png'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
---
+++ lib/configs/colors.dart
@@ -0,0 +1,25 @@
+//
+//  colors
+//  car_dash
+//
+//  Created by Ngonidzashe Mangudya on 23/4/2023.
+//  Copyright (c) 2023 ModestNerds, Co
+//
+
+import 'package:flutter/painting.dart';
+
+class DashColors {
+  static const Color white = Color(0xFFFAFDFF);
+  static const Color dashBackgroundColor1 = Color(0xFF243154);
+  static const Color dashBackgroundColor2 = Color(0xFF131a34);
+  static const Color inactiveControlColor = Color(0xFF1a1d2c);
+  static const Color activeControlColor = Color(0xFF3f5284);
+  static const Color mediaPlayerBackgroundColor = Color(0xFF2c3a61);
+  static const Color mediaDurationBarColor1 = Color(0xFF1c2335);
+  static const Color activeControlChildColor = Color(0xFF94b3f4);
+  static const Color inactiveControlChildColor = Color(0xFF4e5369);
+  static const Color lockedDoorColor = Color(0xFFd4030a);
+  static const Color unlockedDoorColor = Color(0xFF98b5ed);
+  static const Color green = Color(0xFF519763);
+  static const Color backgroundColor = Color(0xFF020514);
+}
